f95572549d180768cb6e3a4883f11171d9967126
Joshua Higgins <joshiggins@gmail.com>
2017-03-22 12:32:58 +0000

    symlink config from repo to actual location instead of copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,32 +26,34 @@ fi
 
 if [[ CONFIG -eq 1 ]]; then
     # reset configuration directories for nagios and nagvis
-    rm -rf ${NAGIOS_ETC}/*
-    rm -rf ${NAGVIS_ETC}/maps/*
-    rm -rf ${NAGVIS_SHARE}/userfiles/*
+    rm -rf ${NAGIOS_ETC}
+    rm -rf ${NAGVIS_ETC}/maps
+    rm -rf ${NAGVIS_SHARE}/userfiles
 
     # clone the git repo
     echo "cloning config repository..."
-    rm -rf /root/nagios-config
-    git clone https://${CONFIG_USER}:${CONFIG_PASS}@${CONFIG_REPO} /root/nagios-config
+    rm -rf /tmp/nagios-config
+    git clone https://${CONFIG_USER}:${CONFIG_PASS}@${CONFIG_REPO} /tmp/nagios-config
 
     # copy nagios config
-    echo "copying config to ${NAGIOS_ETC}"
-    cp -r /root/nagios-config/nagios/* ${NAGIOS_ETC}/
+    echo "linking config to ${NAGIOS_ETC}"
+    ln -s /tmp/nagios-config/nagios ${NAGIOS_ETC}
 
     # copy nagvis maps
-    echo "copying maps to ${NAGVIS_ETC}/maps"
-    cp -r /root/nagios-config/nagvis/maps/* ${NAGVIS_ETC}/maps/
+    echo "linking maps to ${NAGVIS_ETC}/maps"
+    ln -s /tmp/nagios-config/nagvis/maps ${NAGVIS_ETC}/maps
 
     # copy nagvis userfiles
-    echo "copying userfiles to ${NAGVIS_SHARE}/userfiles"
-    cp -r /root/nagios-config/nagvis/userfiles/* ${NAGVIS_SHARE}/userfiles/
+    echo "linking userfiles to ${NAGVIS_SHARE}/userfiles"
+    ln -s /tmp/nagios-config/nagvis/userfiles ${NAGVIS_SHARE}/userfiles
 
     # fix permissions
     echo "fixing permissions"
     chown nagios:nagcmd $NAGIOS_ETC -R
+    chown nagios:nagcmd /tmp/nagios-config/nagios -R
     chown www-data:www-data $NAGVIS_ETC -R
     chown www-data:www-data $NAGVIS_SHARE -R
+    chown www-data:www-data /tmp/nagios-config/nagvis -R
 fi
 
 echo "starting the server"
